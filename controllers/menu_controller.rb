32cef50b1f0861c070e75cbbb220d858256e6e5a
Riley Westbrook <westbrookriley@gmail.com>
2017-11-28 15:43:30 -0500

    assignment 8 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,6 +14,7 @@ class MenuController
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
     puts "5 - Exit"
+    puts "6 - Delete all entries"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -38,6 +39,11 @@ class MenuController
       when 5
         puts "Good-bye!"
         exit(0)
+      when 6
+        system "clear"
+        @address_book.detonate
+        puts "All entries deleted"
+        main_menu
       else
         system "clear"
         puts "Sorry, that is not a valid input"
